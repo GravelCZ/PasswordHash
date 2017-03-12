0e803add058cf798982916f3cf8e93fdb63a67eb
GravelCZLP <jindra.vese@gmail.com>
2017-03-12 14:18:12 +0100

    Better security and fixed typos

--- PasswordHash4j/src/cz/GravelCZLP/PasswordHash/PasswordHashv2.java
+++ PasswordHash4j/src/cz/GravelCZLP/PasswordHash/PasswordHashv2.java
@@ -22,7 +22,7 @@ public class PasswordHashv2 {
 	/**
 	 * basically number of iterations
 	 */
-	public static final int DEFAULT_COST = 128;
+	public static final int DEFAULT_COST = 512;
 	
 	/*
 	 * list of usable peppers, you can add your own if you want to
@@ -30,7 +30,7 @@ public class PasswordHashv2 {
 	private static final char[] pepper = "abcdefghijklopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
 	
 	/**
-	 * Algorithm to use, this is the best i know, PM me if oyu know better one
+	 * Algorithm to use, this is the best i know, PM me if you know better one
 	 */
 	private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
 	
@@ -42,17 +42,17 @@ public class PasswordHashv2 {
 	 * if we split the token by  $
 	 *  we get:
 	 *  $G = our ID
-	 *  $128 = our cost
-	 *  $sIfLCAPmizFnaV1qRqkiZAZdfy3ndff_S32EFtht162OiHu_Eu0_UgKw8JuP0yrAtL8WuFfHxi7c9AypiJRGU44bAJQKQEIXpIKYSN1VF7Fh6R-aV_HEh3Wxu5FrBWSa5hwZSaWBZmn203ggQTB5gJ2iXVlK6pUgiVD0du9j-4I 
+	 *  $512 = our cost
+	 *  $dDGf1rKDRI103VhNZCujG1gL7PNS1sI8hh6z69LTh6hG4KONbmAdDaVFUyuvqGAoiBzdDEZXOfDVfDt202HPDQM6sMU7han2k4Ic0yh0PuXTksq8LFxNKNJap37ICjO1Ljt4NlGdEoXd1k6L8TI0Grx2miCn4P0_G9L6iobi4lbd2z40ZA6eXbo-mTy44OS_WZbe5R1mZTzTEGlGqMcS6as3LxrCBK5F2wsQ_9wrXLIludjKLr2I7Y9GnwMVGTOpS8fZ0wA5kTSWs_jgfZVx1Tn7asqp3p7pJCr9e3ZhJtQaEN-nr0NnaFlkCo65g8dswvKvIy9fRRWpyGSkuz0xxg
 	 *  = our hash
-	 *  The password id P4ssw0rd1
+	 *  The password is P4ssw0rd1
 	 */
-	private static final Pattern layout = Pattern.compile("\\$G\\$(\\d\\d\\d?)\\$(.{171})");
+	private static final Pattern layout = Pattern.compile("\\$G\\$(\\d\\d\\d?)\\$(.{512})");
 	
 	/**
 	 * Size of PBEKeySpec in method pbkdf2(args...)
 	 */
-	private static final int SIZE = 512;
+	private static final int SIZE = 1024;
 
 	/**
 	 * Our Random
@@ -74,7 +74,7 @@ public class PasswordHashv2 {
 	// the ~0x80 should be changed by the cost
 	// use http://numbermonk.com/hexadecimal/128/en to figure out what it should be
 	private static int iterations(int cost) {
-	  if ((cost & ~0x80) != 0) {
+	  if ((cost & ~0x200) != 0) {
 	    throw new IllegalArgumentException("cost: " + cost);
 	  }
 	  return 1 << cost;
@@ -83,18 +83,18 @@ public class PasswordHashv2 {
 	/**
 	 * Hashes password with salt and pepper
 	 * @param password The password to hash
-	 * @return token of salt,pepper(pepper is not stoered),id cost and hash
+	 * @return token of salt,pepper(pepper is not stored),id cost and hash
 	 */
 	public String hash(String password) {
-		byte[] salt = new byte[SIZE / 8]; // size of salt
+		byte[] salt = new byte[SIZE / 4]; // size of salt
 		random.nextBytes(salt); // generate new salt
 		char ppr = pepper[random.nextInt(pepper.length)]; // get random pepper
-		password = password + ppr; // add pepper to passord
+		password = password + ppr; // add pepper to password
 		byte[] dk = pbkdf2(password.toCharArray(), salt, 1 << cost); // hash it
 		byte[] hash = new byte[salt.length + dk.length]; // hash it
 	    System.arraycopy(salt, 0, hash, 0, salt.length); // idk :D
 	    System.arraycopy(dk, 0, hash, salt.length, dk.length); // idk :D
-	    Base64.Encoder enc = Base64.getUrlEncoder().withoutPadding(); // setup enncoder
+	    Base64.Encoder enc = Base64.getUrlEncoder().withoutPadding(); // setup encoder
 	    return ID + cost + "$" + enc.encodeToString(hash); // encode hash and return with all other data
 	}
 	 /**
@@ -104,13 +104,13 @@ public class PasswordHashv2 {
 	  * @return true if passwords match
 	  */
 	public boolean auth(String password, String token) {
-		Matcher m = layout.matcher(token); // setup mathcer
+		Matcher m = layout.matcher(token); // setup matcher
 		if (!m.matches()) { // detect if layout matches
 			throw new IllegalArgumentException("Invalid token");
 		}
 		int iterations = iterations(Integer.parseInt(m.group(1))); // get iterations from token
 		byte[] hash = Base64.getUrlDecoder().decode(m.group(2));  // get hash from token 
-		byte[] salt = Arrays.copyOfRange(hash, 0, SIZE / 8); // get salt from token
+		byte[] salt = Arrays.copyOfRange(hash, 0, SIZE / 4); // get salt from token
 		for (int i = 0; i < pepper.length; i++) { // loop pepper
 			char ppr = pepper[i]; // get next pepper
 			String passw; // init temporary password
